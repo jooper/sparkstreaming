40df389b336a34002f44392020b9da546c4d7ca0
admin1 <joot1314@gmail.com>
2021-09-11 21:35:43 +0800

    重构项目结构

--- src/scala/business/com/lh/utils/PropertiesUtils.scala
+++
@@ -1,9 +0,0 @@
-package com.lh.utils
-
-import java.util.Properties
-
-object PropertiesUtils {
-  val Init = new Properties()
-  private val in = this.getClass().getClassLoader().getResourceAsStream("config.properties");
-  Init.load(in)
-}
